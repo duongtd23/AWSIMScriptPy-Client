1b74eb3f035cf3020dfc291199e2051348410373
Duong Dinh Tran <tranduongthhd@gmail.com>
2025-10-05 17:31:37 +0900

    minor fix

--- scenarios/swerve.py
+++ scenarios/swerve.py
@@ -8,21 +8,22 @@ def make_swerve_scenario(network,
                          ego_goal_laneoffset,
                          npc_init_laneoffset,
                          swerve_start_laneoffset,
-                         ego_speed=30/3.6,
-                         npc_speed=20/3.6,
+                         ego_speed=20/3.6,
+                         npc_speed=10/3.6,
                          swerve_vy = 1.2,
                          swerve_ny=1.8,
                          swerve_dis=2.0,
-                         dx0=30,
+                         dx0=40,
                          npc_root_to_frontcenter=2.01+1.43,
                          swerve_right=True,
-                         acceleration=7):
+                         acceleration=7,
+                         delay_time=0.01):
     _, _, init_pos, init_orient = network.parse_lane_offset(ego_init_laneoffset)
     _, _, goal_pos, goal_orient = network.parse_lane_offset(ego_goal_laneoffset)
 
     # ego specification
     ego = EgoVehicle(node)
-    npc1 = NPCVehicle("npc1", node, BodyStyle.VAN)
+    npc1 = NPCVehicle("npc1", node, BodyStyle.HATCHBACK)
     ego.add_action(SpawnEgo(position=init_pos, orientation=init_orient))
     ego.add_action(SetGoalPose(position=goal_pos, orientation=goal_orient))
     ego.add_action(ActivateAutonomousMode(condition=autonomous_mode_ready()))
@@ -51,18 +52,20 @@ def make_swerve_scenario(network,
     waypoints.append(np.append(wp4, wp1[2]))
 
     # 2nd step: calculate distance to trigger npc movement
-    speedup_time = npc_speed/acceleration
-    speedup_dis = npc_speed**2/2/acceleration
-    remaining_dis_to_wp1 = np.linalg.norm(wp1 - npc_init_pos) - npc_root_to_frontcenter - speedup_dis
-    time_to_wp1 = speedup_time + remaining_dis_to_wp1/npc_speed
+    speedup_time = npc_speed / acceleration
+    speedup_dis = npc_speed ** 2 / 2 / acceleration
+    npc_travel_dis = np.linalg.norm(wp1 - npc_init_pos) - npc_root_to_frontcenter
+    remaining_dis_to_wp1 = npc_travel_dis - speedup_dis
+    time_to_wp1 = speedup_time + remaining_dis_to_wp1 / npc_speed + delay_time
     ego_travel_dis = ego_speed * time_to_wp1
-    dis_threshold = dx0 + ego_travel_dis + np.linalg.norm(wp1 - npc_init_pos) - npc_root_to_frontcenter
+    dis_threshold = dx0 + ego_travel_dis + npc_travel_dis
+    print(dis_threshold)
 
     # npc and follow waypoints (swerve waypoints) specification
     npc1.add_action(SpawnNPCVehicle(position=npc_init_pos, orientation=npc_init_orient))
     npc1.add_action(FollowWaypoints(waypoints=[utils.array_to_dict_pos(p) for p in waypoints],
                                     target_speed=npc_speed,
-                                    acceleration=7,
+                                    acceleration=8,
                                     condition=longitudinal_distance_to_ego_less_than(dis_threshold)))
 
     return ScenarioManager(node, network,[ego, npc1])
@@ -76,8 +79,8 @@ if __name__ == '__main__':
     scenario = make_swerve_scenario(network,
                                     ego_init_laneoffset=LaneOffset('355', 20),
                                     ego_goal_laneoffset=LaneOffset('214', 21),
-                                    npc_init_laneoffset=LaneOffset('205', 50),
-                                    swerve_start_laneoffset=LaneOffset('205', 58)
+                                    npc_init_laneoffset=LaneOffset('205', 40),
+                                    swerve_start_laneoffset=LaneOffset('205', 48)
                                     )
     scenario.run()
 
